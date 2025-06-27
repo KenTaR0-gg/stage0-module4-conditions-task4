6337955be20caf9ae447f7b871a0fa0f5b4d591b
KenTaR0-gg <kotya.piso@gmail.com>
2025-06-27 19:32:43 +0300

    module 4. task 4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -1,6 +1,10 @@
 package school.mjc.stage0.conditions.task4;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        int result = first>second ? 10: -10;
+        System.out.println(result);
     }
 }
